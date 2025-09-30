02721a8fb5eecd31a276e63e3b08d85b2038ba77
yuyongkang <neonkk@qq.com>
2025-10-01 06:33:56 +0800

    Prune darwin build script

--- scripts/darwin-build.sh
+++ scripts/darwin-build.sh
@@ -18,20 +18,13 @@ export GO111MODULE=on
 export GOPROXY=https://mirrors.aliyun.com/goproxy/
 export CGO_ENABLED=1
 
-echo 'Building Kernel amd64'
-export GOOS=darwin
-export GOARCH=amd64
-go build --tags fts5 -v -o "../app/kernel-darwin/SiYuan-Kernel" -ldflags "-s -w" .
-
 echo 'Building Kernel arm64'
 export GOOS=darwin
 export GOARCH=arm64
 go build --tags fts5 -v -o "../app/kernel-darwin-arm64/SiYuan-Kernel" -ldflags "-s -w" .
 cd ..
 
-echo 'Building Electron App amd64'
 cd app
-pnpm run dist-darwin
 echo 'Building Electron App arm64'
 pnpm run dist-darwin-arm64
 cd ..
